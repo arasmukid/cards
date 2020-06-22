05db445e89faaa58243e15fa8f752b2918ce5576
arasmukid <arasmukid@yahoo.com>
2020-06-22 20:22:58 +0700

    belajar REFERENCE

--- deck.go
+++ deck.go
@@ -8,7 +8,8 @@ type deck []string
 
 // create function specific for this deck data type
 
-func (d deck) print() {
+func (d deck) print() { // (d deck) adalah referrence variable ke data typenya shg si variable
+	// bisa mengakses print method,
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
