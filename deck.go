edb07c27c77d765db3b345dbfb278ebb6dede9dc
arasmukid <arasmukid@yahoo.com>
2020-06-22 20:47:34 +0700

    belajar bikin newDeck semacam constructor dari datatype deck, belajar nested for loop

--- deck.go
+++ deck.go
@@ -6,10 +6,28 @@ import "fmt"
 
 type deck []string
 
+// create newDeck functin semacam constructor
+
+func newDeck() deck {
+
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 // create function specific for this deck data type
 
 func (d deck) print() { // (d deck) adalah referrence variable ke data typenya shg si variable
-	// bisa mengakses print method,
+	// bisa mengakses print method
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
